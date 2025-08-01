dd22de359986c5c195a462139f5fdf48e80ef518
junhwik <wjdrhdsbrlfl@gmail.com>
2025-08-01 12:44:22 +0900

    [refactor] p.21

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -1,45 +1,62 @@
 import pytest
 
 from schedule import Customer, Schedule
+
+CAPACITY_PER_HOUR = 3
+
+UNDER_CAPACITY = 1
+
+TEST_CUSTOMER = Customer("asdf", "010-1111-2222")
+
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
 from datetime import datetime
 
-def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가():
+TEST_TIME = datetime.strptime("2025/08/01 09:00", "%Y/%m/%d %H:%M")
+
+@pytest.fixture
+def booking_scheduler():
+    return BookingScheduler(CAPACITY_PER_HOUR)
+
+def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(booking_scheduler):
     test_time = datetime.strptime("2025/08/01 09:05", "%Y/%m/%d %H:%M")
     customer = Customer("asdf", "010-1111-2222")
     schedule = Schedule(test_time, 1, customer)
-    booking_scheduler = BookingScheduler(3)
 
     with pytest.raises(ValueError):
         booking_scheduler.add_schedule(schedule)
 
-def test_예약은_정시에만_가능하다_정시인_경우_예약가능():
-    test_time = datetime.strptime("2025/08/01 09:00", "%Y/%m/%d %H:%M")
-    customer = Customer("asdf", "010-1111-2222")
-    schedule = Schedule(test_time, 1, customer)
-    booking_scheduler = BookingScheduler(3)
+
+def test_예약은_정시에만_가능하다_정시인_경우_예약가능(booking_scheduler):
+    schedule = Schedule(TEST_TIME, UNDER_CAPACITY, TEST_CUSTOMER)
 
     booking_scheduler.add_schedule(schedule)
     assert booking_scheduler.has_schedule(schedule)
 
+
 def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생():
     pass
+
 
 def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공():
     pass
 
+
 def test_예약완료시_SMS는_무조건_발송():
     pass
+
 
 def test_이메일이_없는_경우에는_이메일_미발송():
     pass
 
+
 def test_이메일이_있는_경우에는_이메일_발송():
     pass
 
+
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
     pass
+
 
 def test_현재날짜가_일요일이_아닌경우_예약가능():
     pass
